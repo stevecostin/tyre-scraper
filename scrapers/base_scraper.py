79c88bff5bb8a1b76ba5eb0feae4dc868dd96b2f
Steve Costin <steve@thecostins.uk>
2025-11-30 02:53:29 +0000

    Changed CSV methods to static methods

--- scrapers/base_scraper.py
+++ scrapers/base_scraper.py
@@ -48,34 +48,37 @@ class BaseScraper(ABC):
         """
         pass
 
+    @staticmethod
+    def get_csv_filename() -> str:
+        """
+        Returns:
+            str: The name of the CSV file that will be used for writing data to (e.g. [domain].csv).
+        """
+        return "tyre_scrape.csv"
+
+    @staticmethod
+    def write_to_csv_file(retailer: str, tyres: list[Tyre]) -> None:
+        """
+        Writes each Tyre entry to a CSV file named after the URL (e.g. www.website.com.csv).
+        If the file doesn't exist or is empty when it's opened for writing it will populate the first line with the csv headers, otherwise it will just write the tyre data.
+
+        Args:
+            retailer (str): The name of the retailer that was scraped
+            tyres (list[Tyre]): The list of Tyres to be written to the file.
+        """
+        filename: str = BaseScraper.get_csv_filename()
+        file_exists: bool = os.path.exists(filename) and os.path.getsize(filename) > 0
+
+        with open(filename, "a", encoding='utf-8') as f:
+            if not file_exists:
+                f.write(f"retailer,{Tyre.get_tyre_attribute_names()}\n")
+
+            for tyre in tyres:
+                f.write(f"{retailer},{tyre}\n")
+
     def get_basic_tyre_details(self) -> str:
         """
         Returns:
             str: The tyre width, aspect ratio and rim diameter in a friendly format (e.g. 205/55/R16).
         """
         return f"{self.tyre_width}/{self.aspect_ratio}/R{self.rim_diameter}"
-
-    def get_csv_filename(self) -> str:
-        """
-        Returns:
-            str: The name of the CSV file that will be used for writing data to (e.g. [domain].csv).
-        """
-        return f"{self.domain}.csv"
-
-    def write_to_csv_file(self, tyres: list[Tyre]) -> None:
-        """
-        Writes each Tyre entry to a CSV file named after the URL (e.g. www.website.com.csv).
-        If the file doesn't exist or is empty when it's opened for writing it will populate the first line with the csv headers, otherwise it will just write the tyre data.
-
-        Args:
-            tyres (list[Tyre]): The list of Tyres to be written to the file.
-        """
-        filename: str = self.get_csv_filename()
-        file_exists: bool = os.path.exists(filename) and os.path.getsize(filename) > 0
-
-        with open(filename, "a", encoding='utf-8') as f:
-            if not file_exists:
-                f.write(Tyre.get_tyre_attribute_names() + '\n')
-
-            for tyre in tyres:
-                f.write(str(tyre) + '\n')
