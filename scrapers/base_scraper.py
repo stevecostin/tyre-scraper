7ce5ee06b3bf1cf9336ee6470daf942c2441f4ba
Steve Costin <steve@thecostins.uk>
2025-11-30 01:30:43 +0000

    Rearranged the methods

--- scrapers/base_scraper.py
+++ scrapers/base_scraper.py
@@ -17,24 +17,6 @@ class BaseScraper(ABC):
         self.rim_diameter = rim_diameter
         self.domain = self.get_url().replace('https://', '').replace('http://', '').split('/')[0] # Removes any http:// or https:// from the beginning of the URL
 
-    def write_to_csv_file(self, tyres: list[Tyre]) -> None:
-        """
-        Writes each Tyre entry to a CSV file named after the URL (e.g. www.website.com.csv).
-        If the file doesn't exist when it's opened for writing it will populate the first line with the csv headers, otherwise it will just write the tyre data.
-
-        Args:
-            tyres (list[Tyre]): The list of Tyres to be written to the file.
-        """
-        filename: str = self.get_csv_filename()
-        file_exists: bool = os.path.exists(filename)
-
-        with open(self.get_csv_filename(), "a", encoding='utf-8') as f:
-            if not file_exists:
-                f.write(Tyre.get_tyre_attribute_names() + '\n')
-
-            for tyre in tyres:
-                f.write(str(tyre) + '\n')
-
     @abstractmethod
     def get_url(self) -> str:
         """
@@ -53,6 +35,19 @@ class BaseScraper(ABC):
         """
         pass
 
+    @abstractmethod
+    def scrape(self) -> list[Tyre]:
+        """
+        Starts the scraping process on the URL provided by the get_request_url() method.
+
+        Returns:
+            list[Tyre]: The list of Tyres that have been scraped from the website.
+
+        Raises:
+            RequestException: There was a problem with the connection to the website
+        """
+        pass
+
     def get_basic_tyre_details(self) -> str:
         """
         Returns:
@@ -67,15 +62,20 @@ class BaseScraper(ABC):
         """
         return f"{self.domain}.csv"
 
-    @abstractmethod
-    def scrape(self) -> list[Tyre]:
+    def write_to_csv_file(self, tyres: list[Tyre]) -> None:
         """
-        Starts the scraping process on the URL provided by the get_request_url() method.
+        Writes each Tyre entry to a CSV file named after the URL (e.g. www.website.com.csv).
+        If the file doesn't exist when it's opened for writing it will populate the first line with the csv headers, otherwise it will just write the tyre data.
 
-        Returns:
-            list[Tyre]: The list of Tyres that have been scraped from the website.
-
-        Raises:
-            RequestException: There was a problem with the connection to the website
+        Args:
+            tyres (list[Tyre]): The list of Tyres to be written to the file.
         """
-        pass
+        filename: str = self.get_csv_filename()
+        file_exists: bool = os.path.exists(filename)
+
+        with open(self.get_csv_filename(), "a", encoding='utf-8') as f:
+            if not file_exists:
+                f.write(Tyre.get_tyre_attribute_names() + '\n')
+
+            for tyre in tyres:
+                f.write(str(tyre) + '\n')
